af1a7f477a2dfb922e339acfb8e1eaf81d2837c5
pavels-s <pavels.sarkels@gmail.com>
2022-09-13 14:22:35 +0300

    Pause while testing

--- tests/example_helpers.spec.ts
+++ tests/example_helpers.spec.ts
@@ -4,6 +4,7 @@ import { loadHomePage, assertTitle } from '../helpers'
 //npx playwright test tests/example_helpers.spec.ts --config=playwright.config.ts --project=Chromium
 
 test("Simple test using helpers", async ({ page }) => {
-await loadHomePage(page)
-await assertTitle(page)
+    await loadHomePage(page)
+    await page.pause()
+    await assertTitle(page)
 })
