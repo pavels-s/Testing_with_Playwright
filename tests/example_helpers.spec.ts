8b03222fa329727b123fd7bf91b87a0926cccd90
pavels-s <pavels.sarkels@gmail.com>
2022-09-13 12:23:41 +0300

    Adding helpers

---
+++ tests/example_helpers.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test'
+import { loadHomePage, assertTitle } from '../helpers'
+
+//npx playwright test tests/example_helpers.spec.ts --config=playwright.config.ts --project=Chromium
+
+test("Simple test using helpers", async ({ page }) => {
+await loadHomePage(page)
+await assertTitle(page)
+})
